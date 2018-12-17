ed72c952385bd7d8aa5596ddfba3b9c2b5dac3f4
YT-Amian <38012056+YT-Amian@users.noreply.github.com>
2018-12-17 18:39:25 +0800

    py查找
    啦啊来啦

---
+++ webapp/vsearch4web.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, escape
+from finding import find
+
+app = Flask(__name__)
+
+
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    """Extract the posted data; perform the search; return results."""
+    phrase = request.form['phrase']
+    title = 'Here are your results:'
+    results = find(phrase)
+    titles = ('name','id','github','segmentfault','email')
+    return render_template('view.html',
+                           the_title='搜索结果',
+                           the_row_titles=titles,
+                           the_data=results,)
+
+
+@app.route('/')
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    """Display the contents of the log file as a HTML table."""
+    contents = []
+    with open('records.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split(','):
+                contents[-1].append(escape(item))
+    titles = ('username', 'lastname', 'firstname', 'id', 'github', 'segmentfault', 'email')
+    return render_template('viewlog.html',
+                           the_title='搜索',
+                           the_title2='班级表单',
+                           the_row_titles=titles,
+                           the_data=contents,)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
